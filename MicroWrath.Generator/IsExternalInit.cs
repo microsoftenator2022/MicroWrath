3a891fc26a7ebabba6cf82764a8d4ea28a4aac4d
Aaron Maslen <aaron.maslen@gmail.com>
2023-04-08 11:55:13 +1000

    Add the IsExternalInit polyfill (for record structs and init-only initializers) to generator output

--- MicroWrath.Generator/IsExternalInit.cs
+++ MicroWrath.Generator/IsExternalInit.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
+
 
 namespace System.Runtime.CompilerServices
 {
     [DebuggerNonUserCode]
-    internal static class IsExternalInit { }
+    internal static partial class IsExternalInit { }
 }
