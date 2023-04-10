607e41d393b9f3ced2cee6d3c8ca8b16e0ec7e20
Aaron Maslen <aaron.maslen@gmail.com>
2023-04-10 12:17:28 +1000

    IBlueprintConstructor should be invariant.

--- MicroWrath.Generator/Resources/Constructor.cs
+++ MicroWrath.Generator/Resources/Constructor.cs
@@ -6,25 +6,27 @@ namespace MicroWrath.Constructors
 {
     internal static partial class Construct
     {
-        private interface IBlueprintConstructor<out TBlueprint> where TBlueprint : SimpleBlueprint
+        private interface IBlueprintConstructor<TBlueprint> where TBlueprint : SimpleBlueprint, new()
         {
             TBlueprint New(string assetId, string name);
         }
 
         private partial class BlueprintConstructor : IBlueprintConstructor<SimpleBlueprint>
         {
-            internal BlueprintConstructor() {  }
+            internal BlueprintConstructor() { }
 
             SimpleBlueprint IBlueprintConstructor<SimpleBlueprint>.New(string assetId, string name) =>
                 new() { AssetGuid = BlueprintGuid.Parse(assetId), name = name };
 
-            public TBlueprint New<TBlueprint>(string assetId, string name) where TBlueprint : SimpleBlueprint =>
+            public TBlueprint New<TBlueprint>(string assetId, string name) where TBlueprint : SimpleBlueprint, new() =>
                 ((IBlueprintConstructor<TBlueprint>)this).New(assetId, name);
         }
 
         public static class New
         {
             private static readonly Lazy<BlueprintConstructor> blueprintConstructor = new(() => new());
+            public static TBlueprint Blueprint<TBlueprint>(string assetId, string name) where TBlueprint : SimpleBlueprint, new() =>
+                blueprintConstructor.Value.New<TBlueprint>(assetId, name);
 
             public static TBlueprint Blueprint<TBlueprint> (string assetId, string name) where TBlueprint : SimpleBlueprint =>
                 blueprintConstructor.Value.New<TBlueprint>(assetId, name);
