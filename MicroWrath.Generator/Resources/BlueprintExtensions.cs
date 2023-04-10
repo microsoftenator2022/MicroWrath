48dc012528c78a27dd03811f0256eefff6a9a332
Aaron Maslen <aaron.maslen@gmail.com>
2023-04-10 15:20:03 +1000

    AddComponent extension method

---
+++ MicroWrath.Generator/Resources/BlueprintExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Kingmaker.Blueprints;
+
+using MicroWrath.Util;
+using MicroWrath.Util.Linq;
+
+namespace MicroWrath
+{
+    internal static class BlueprintExtensions
+    {
+        public static void AddComponent(this BlueprintScriptableObject blueprint, BlueprintComponent component)
+        {
+            var name =
+                string.IsNullOrEmpty(component.name) ?
+                $"${blueprint.name ?? blueprint.GetType().Name}${component.GetType().Name}" :
+                component.name;
+
+            component.name = name;
+
+            for (var i = 2; blueprint.ComponentsArray.Select(c => c.name).Contains(component.name); i++)
+                component.name = $"{name}${i}";
+
+            blueprint.ComponentsArray = blueprint.ComponentsArray.Append(component);
+        }
+    }
+}
