1a4f3b930e038f0f1cf0b11f212071b3619e70c7
Aaron Maslen <aaron.maslen@gmail.com>
2023-04-19 01:00:14 +1000

    Added ModResources for resources that should be added to Mods' namespaces instead of MicroWrath

--- MicroWrath.Generator/EmbeddedResources.cs
+++ MicroWrath.Generator/EmbeddedResources.cs
@@ -7,23 +7,54 @@ using System.Text;
 
 using Microsoft.CodeAnalysis;
 
+using MicroWrath.Generator.Common;
+using MicroWrath.Util;
+
 namespace MicroWrath.Generator
 {
     [Generator]
     internal class EmbeddedResources : IIncrementalGenerator
     {
+        private static IEnumerable<string> GetGeneratorResourceNames(Assembly assembly) =>
+            assembly.GetManifestResourceNames().Where(n => n.EndsWith(".cs"));
+
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
-            context.RegisterPostInitializationOutput(pic => 
+            context.RegisterPostInitializationOutput(static pic => 
             {
-                foreach (var name in Assembly.GetExecutingAssembly().GetManifestResourceNames().Where(n => n.EndsWith(".cs")))
+                foreach (var name in GetGeneratorResourceNames(Assembly.GetExecutingAssembly())
+                    .Where(name => name.StartsWith($"{nameof(MicroWrath)}.{nameof(Generator)}.Resources.")))
                 {
                     using var sr = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(name));
                     var str = sr.ReadToEnd();
 
-                    var sourceName = name.Replace($"{nameof(MicroWrath.Generator)}.", "").Replace("Resources.", "");
+                    var sourceName = name
+                        .Replace($"{nameof(Generator)}.", "")
+                        .Replace("Resources.", "");
 
                     pic.AddSource(sourceName, str);
+                }
+            });
+
+            var config = context.AnalyzerConfigOptionsProvider.Select(static (config, _) => Incremental.GetConfig(config));
+            var rootNamespace = config.Select(static (config, _) => config.RootNamespace);
+
+            context.RegisterSourceOutput(rootNamespace, (spc, ns) =>
+            {
+                if (ns.IsNone) return;
+
+                foreach (var name in GetGeneratorResourceNames(Assembly.GetExecutingAssembly())
+                    .Where(name => name.StartsWith($"{nameof(MicroWrath)}.{nameof(Generator)}.ModResources.")))
+                {
+                    using var sr = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(name));
+
+                    var str = sr.ReadToEnd().Replace($"namespace {nameof(MicroWrath)}", $"namespace {ns.Value!}");
+
+                    var sourceName = name
+                        .Replace($"{nameof(MicroWrath)}.{nameof(MicroWrath.Generator)}.", "")
+                        .Replace("ModResources.", "");
+
+                    spc.AddSource(sourceName, str);
                 }
             });
         }
