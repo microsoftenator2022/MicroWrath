57c93a900d64f2b708f6d20854375ff633107a93
Aaron Maslen <aaron.maslen@gmail.com>
2023-04-24 17:44:35 +1000

    Rename ContextInitializer.Select to Map

--- MicroWrath/BlueprintInitializationContext/BlueprintInitializationContext.Initializer.cs
+++ MicroWrath/BlueprintInitializationContext/BlueprintInitializationContext.Initializer.cs
@@ -14,13 +14,13 @@ namespace MicroWrath.BlueprintInitializationContext
         internal static BlueprintInitializationContext.ContextInitializer<TOther> Combine<TOther>(
             this BlueprintInitializationContext.ContextInitializer obj,
             BlueprintInitializationContext.ContextInitializer<TOther> other) =>
-            obj.Select(() => new object()).Combine(other).Select(x => x.Item2);
+            obj.Map(() => new object()).Combine(other).Map(x => x.Item2);
 
         internal static BlueprintInitializationContext.ContextInitializer<TBlueprint> GetBlueprint<TBlueprint>(
             this BlueprintInitializationContext.ContextInitializer obj,
             IMicroBlueprint<TBlueprint> blueprint)
             where TBlueprint : SimpleBlueprint =>
-            obj.Select(() => new object()).GetBlueprint(blueprint).Select(x => x.Item2);
+            obj.Map(() => new object()).GetBlueprint(blueprint).Map(x => x.Item2);
 
         internal static BlueprintInitializationContext.ContextInitializer<IEnumerable<TBlueprint>> Combine<TBlueprint>(
             this IEnumerable<BlueprintInitializationContext.ContextInitializer<TBlueprint>> bpcs)
@@ -30,8 +30,8 @@ namespace MicroWrath.BlueprintInitializationContext
             var tail = bpcs.Skip(1);
 
             return tail.Aggregate(
-                head.Select(EnumerableExtensions.Singleton),
-                (acc, next) => acc.Combine(next).Select(x => x.Item1.Append(x.Item2)));
+                head.Map(EnumerableExtensions.Singleton),
+                (acc, next) => acc.Combine(next).Map(x => x.Item1.Append(x.Item2)));
         }
     }
 
@@ -40,15 +40,15 @@ namespace MicroWrath.BlueprintInitializationContext
         internal abstract class ContextInitializer
         {
             protected abstract BlueprintInitializationContext InitContext { get; }
-            public abstract ContextInitializer Select(Action action);
-            public abstract ContextInitializer<TResult> Select<TResult>(Func<TResult> selector);
+            public abstract ContextInitializer Map(Action action);
+            public abstract ContextInitializer<TResult> Map<TResult>(Func<TResult> selector);
             public abstract void Register();
         }
 
         internal abstract class ContextInitializer<T> : ContextInitializer
         {
-            public abstract ContextInitializer Select(Action<T> action);
-            public abstract ContextInitializer<TResult> Select<TResult>(Func<T, TResult> selector);
+            public abstract ContextInitializer Map(Action<T> action);
+            public abstract ContextInitializer<TResult> Map<TResult>(Func<T, TResult> selector);
             public abstract ContextInitializer<(T, TOther)> Combine<TOther>(ContextInitializer<TOther> other);
 
             public virtual ContextInitializer<(T, TBlueprint)> GetBlueprint<TBlueprint>(IMicroBlueprint<TBlueprint> blueprint)
@@ -112,24 +112,24 @@ namespace MicroWrath.BlueprintInitializationContext
 
             private BlueprintInit<TResult> With<TResult>(Func<TResult> getValue) => new(initContext, Blueprints, getValue);
 
-            public override ContextInitializer<TResult> Select<TResult>(Func<T, TResult> selector) =>
+            public override ContextInitializer<TResult> Map<TResult>(Func<T, TResult> selector) =>
                 With(() => selector(Value));
 
-            public override ContextInitializer<TResult> Select<TResult>(Func<TResult> selector) =>
+            public override ContextInitializer<TResult> Map<TResult>(Func<TResult> selector) =>
                 With(() =>
                 {
                     GetValue();
                     return selector();
                 });
 
-            public override ContextInitializer Select(Action<T> action) =>
+            public override ContextInitializer Map(Action<T> action) =>
                 With<object>(() =>
                 {
                     action(Value);
                     return new();
                 });
 
-            public override ContextInitializer Select(Action action) =>
+            public override ContextInitializer Map(Action action) =>
                 With<object>(() =>
                 {
                     GetValue();
