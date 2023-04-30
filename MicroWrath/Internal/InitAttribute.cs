99922a9b582146680e7c327f86067f1b62736051
Aaron Maslen <aaron.maslen@gmail.com>
2023-04-30 23:55:57 +1000

    Fix namespace for InitAtrribute

--- MicroWrath/Internal/InitAttribute.cs
+++ MicroWrath/Internal/InitAttribute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MiscTweaksAndFixes
+namespace MicroWrath
 {
     [AttributeUsage(validOn: AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     internal sealed class InitAttribute : Attribute { }
