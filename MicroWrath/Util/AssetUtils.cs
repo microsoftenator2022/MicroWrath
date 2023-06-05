974295dd9964f6ec6661d8eca0595303848dcec8
Aaron Maslen <aaron.maslen@gmail.com>
2023-06-05 20:57:17 +1000

    Add TTT's ObjectDeepCopy + new CloneBlueprint helper

--- MicroWrath/Util/AssetUtils.cs
+++ MicroWrath/Util/AssetUtils.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.JsonSystem.Converters;
+
+using TabletopTweaks.Core.Utilities;
+
 using UnityEngine;
 
 namespace MicroWrath.Util
@@ -15,6 +19,19 @@ namespace MicroWrath.Util
         {
             public static Sprite GetSprite(string assetId, long fileId) =>
                 (Sprite)UnityObjectConverter.AssetList.Get(assetId, fileId);
+        }
+
+        public static TBlueprint CloneBlueprint<TBlueprint>(TBlueprint blueprint, BlueprintGuid guid, string? name = null, bool addToLibrary = true) where TBlueprint : SimpleBlueprint
+        {
+            blueprint = (ObjectDeepCopier.Clone(blueprint) as TBlueprint)!;
+
+            blueprint.AssetGuid = guid;
+
+            if (name is not null) blueprint.name = name;
+
+            if (addToLibrary) ResourcesLibrary.BlueprintsCache.AddCachedBlueprint(blueprint.AssetGuid, blueprint);
+
+            return blueprint;
         }
     }
 }
