6d9491bde495a5432574a0b33b4ec5687e11ea41
Aaron Maslen <aaron.maslen@gmail.com>
2023-08-01 15:03:28 +1000

    Added execution time debug logging to Triggers

--- MicroWrath/ModInternal/Triggers.cs
+++ MicroWrath/ModInternal/Triggers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,9 @@ using Kingmaker.Blueprints.JsonSystem;
 using Kingmaker.Localization;
 using Kingmaker.Localization.Shared;
 
-using UniRx;
-
 using MicroWrath;
+
+using UniRx;
 
 namespace MicroWrath
 {
@@ -26,11 +27,23 @@ namespace MicroWrath
         [HarmonyPostfix]
         private static void BlueprintsCache_Init_Patch()
         {
+            var timer = new Stopwatch();
+
             MicroLogger.Debug(() => $"Trigger {nameof(BlueprintsCache_Init_Early)}");
+            timer.Restart();
+
             BlueprintsCache_InitEvent_Early();
 
+            timer.Stop();
+            MicroLogger.Debug(() => $"Trigger {nameof(BlueprintsCache_Init_Early)} completed in {timer.ElapsedMilliseconds}ms");
+
             MicroLogger.Debug(() => $"Trigger {nameof(BlueprintsCache_Init)}");
+            timer.Restart();
+
             BlueprintsCache_InitEvent();
+
+            MicroLogger.Debug(() => $"Trigger {nameof(BlueprintsCache_Init)} completed in {timer.ElapsedMilliseconds}ms");
+            timer.Stop();
         }
 
         public static readonly IObservable<Unit> BlueprintsCache_Init_Early =
@@ -49,8 +62,14 @@ namespace MicroWrath
         [HarmonyPrefix]
         private static void SwitchLanguage_Patch()
         {
+            var timer = new Stopwatch();
+
             MicroLogger.Debug(() => $"Trigger {nameof(LocaleChanged)}");
+            timer.Restart();
+
             LocalizationManager_OnLocaleChangedEvent(LocalizationManager.CurrentLocale);
+            timer.Stop();
+            MicroLogger.Debug(() => $"Trigger {nameof(LocaleChanged)} completed in {timer.ElapsedMilliseconds}ms");
         }
 
         public static readonly IObservable<Locale> LocaleChanged =
