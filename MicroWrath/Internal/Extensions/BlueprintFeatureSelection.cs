cf98bb0d5d7bcc21763119fa8566c1e80f090050
Aaron Maslen <aaron.maslen@gmail.com>
2023-11-26 17:03:48 +1100

    Split Blueprint extensions for BlueprintFeatureSelection and BlueprintSpellList into their own files.
    Refactor BlueprintFeatureSelection.AddFeatures extension methods to use BlueprintFeatureReference (instead of IMicroBlueprint<BlueprintFeature>)

---
+++ MicroWrath/Internal/Extensions/BlueprintFeatureSelection.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kingmaker.Blueprints.Classes.Selection;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints;
+using MicroWrath.Util.Linq;
+
+namespace MicroWrath.Extensions
+{
+    internal static partial class BlueprintExtensions
+    {
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            bool allowDuplicates,
+            IEnumerable<BlueprintReference<TBlueprint>> features)
+            where TBlueprint : BlueprintFeature
+        {
+            var featuresList = selection.m_Features.ToList();
+            var allFeaturesList = selection.m_AllFeatures.ToList();
+
+            foreach (var bpRef in features)
+            {
+                MicroLogger.Debug(() => $"Adding {bpRef} to selection {selection.name}", selection.ToMicroBlueprint());
+                if (allowDuplicates || !featuresList.Any(f => f.deserializedGuid == bpRef.deserializedGuid))
+                    featuresList.Add(new BlueprintFeatureReference() { deserializedGuid = bpRef.deserializedGuid });
+
+                if (allowDuplicates || !allFeaturesList.Any(f => f.deserializedGuid == bpRef.deserializedGuid))
+                    allFeaturesList.Add(new BlueprintFeatureReference() { deserializedGuid = bpRef.deserializedGuid });
+            }
+
+            selection.m_Features = featuresList.ToArray();
+            selection.m_AllFeatures = allFeaturesList.ToArray();
+        }
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            IEnumerable<BlueprintReference<TBlueprint>> blueprints)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, false, blueprints);
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            bool allowDuplicates,
+            BlueprintReference<TBlueprint> feature,
+            params BlueprintReference<TBlueprint>[] features)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, allowDuplicates, [feature, .. features]);
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            BlueprintReference<TBlueprint> feature,
+            params BlueprintReference<TBlueprint>[] features)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, false, feature, features);
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            bool allowDuplicates,
+            IEnumerable<TBlueprint> features)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, allowDuplicates, features.Select(bp => bp.ToReference<BlueprintReference<TBlueprint>>()));
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            IEnumerable<TBlueprint> blueprints)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, false, blueprints);
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            bool allowDuplicates,
+            TBlueprint feature,
+            params TBlueprint[] features)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, allowDuplicates, [feature, .. features]);
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            TBlueprint feature,
+            params TBlueprint[] features)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, false, feature, features);
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            bool allowDuplicates,
+            IEnumerable<IMicroBlueprint<TBlueprint>> features)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, allowDuplicates, features.Select(bp => bp.ToReference<TBlueprint, BlueprintReference<TBlueprint>>()));
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            IEnumerable<IMicroBlueprint<TBlueprint>> blueprints)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, false, blueprints);
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            bool allowDuplicates,
+            IMicroBlueprint<TBlueprint> feature,
+            params IMicroBlueprint<TBlueprint>[] features)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, allowDuplicates, [feature, .. features]);
+
+        public static void AddFeatures<TBlueprint>(
+            this BlueprintFeatureSelection selection,
+            IMicroBlueprint<TBlueprint> feature,
+            params IMicroBlueprint<TBlueprint>[] features)
+            where TBlueprint : BlueprintFeature =>
+            AddFeatures(selection, false, feature, features);
+    }
+}
---
+++ MicroWrath/Internal/Extensions/BlueprintSpellList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+
+using MicroWrath.Util.Linq;
+
+namespace MicroWrath.Extensions
+{
+    internal static partial class BlueprintExtensions
+    {
+        public static void AddToSpellList(this BlueprintAbility spell, BlueprintSpellList spellList, int level)
+        {
+            var slc = spell.Components.OfType<SpellListComponent>().FirstOrDefault();
+
+            MicroLogger.Debug(() => $"Adding {spell} to {spellList} level {level}");
+
+            slc ??= spell.AddComponent<SpellListComponent>(c =>
+            {
+                c.m_SpellList = spellList.ToReference<BlueprintSpellListReference>();
+                c.SpellLevel = level;
+            });
+
+            if (slc.SpellLevel != level)
+            {
+                MicroLogger.Warning($"{spell} level for {spellList} is {slc.SpellLevel}, but added to {level}");
+            }
+
+            var spellListForLevel = spellList.SpellsByLevel.FirstOrDefault(sl => sl.SpellLevel == level);
+            if (spellListForLevel is null)
+            {
+                spellListForLevel = new SpellLevelList(level);
+
+                spellList.SpellsByLevel = spellList.SpellsByLevel.Append(spellListForLevel);
+            }
+
+            spellListForLevel.m_Spells.Add(spell.ToReference<BlueprintAbilityReference>());
+        }
+
+        public static void AddToSpellLists(this BlueprintAbility spell, IEnumerable<(BlueprintSpellList, int)> spellLists)
+        {
+            foreach (var (sl, level) in spellLists)
+                spell.AddToSpellList(sl, level);
+        }
+
+        public static void AddToSpellLists(this BlueprintAbility spell) =>
+            spell.AddToSpellLists(spell.Components
+                .OfType<SpellListComponent>()
+                .Select(c => (c.SpellList, c.SpellLevel)));
+    }
+}
